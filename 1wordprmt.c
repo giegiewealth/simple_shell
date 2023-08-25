75018775a63c923cfcb0dd418af4bf099c1b811d
giegiewealth <angelamarfo17@gmail.com>
2023-08-24 20:06:35 -0700

    redo

--- 1wordprmt.c
+++
@@ -1,7 +0,0 @@
-#include "mainshell.h"
-
-void display_prompt(void)
-{
-    	print_dng("dan_n_gie_shell$ ");
-}
-
--- dng_print.c
+++
@@ -1,6 +0,0 @@
-#include "mainshell.h"
-
-void print_dng(const char *message)
-{
-    write(STDOUT_FILENO, message, strlen(message));
-}
